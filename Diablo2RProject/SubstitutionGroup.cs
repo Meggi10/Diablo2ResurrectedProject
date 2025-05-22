9213a630038c0baaf2b87f1e460c3c17b28e8427
Magdalena Leśniewska <119002735+Meggi10@users.noreply.github.com>
2025-05-22 20:18:18 +0200

    Add files via upload
    Dane z plików .ds1 są odczytywane w poprawny sposób. Na razie jest tylko samo wyświetlenie tych struktur głównie kolorami bez innych rzeczy (kolory służą na razie do identyfikacji LowerWall, UpperWall oraz Special).

---
+++ Diablo2RProject/SubstitutionGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diablo2RProject
+{
+    public class SubstitutionGroup
+    {
+        public int TileX;
+        public int TileY;
+        public int WidthInTiles;
+        public int HeightInTiles;
+        public int Unknown;
+    }
+}
