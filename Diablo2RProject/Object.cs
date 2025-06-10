593b7bd1c847475226342849990aee44730f4c4c
Magdalena Leśniewska <119002735+Meggi10@users.noreply.github.com>
2025-06-10 19:53:04 +0200

    Update projektu z dnia 10.06.2025r.
    Projekt wrzucany aby nie zapomnieć na której wersji się pracuje.
    
    TODO: połączyć pliki *.ds1 z *.dt1 tak aby ładować jeden plik *.ds1 z widocznymi już TileSets.

---
+++ Diablo2RProject/Object.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diablo2RProject
+{
+    public class Object: Path
+    {
+        public int Type;
+        public int Id;
+        public int X;
+        public int Y;
+        public int Flag;
+        public List<Path>Paths { get; set; } = new List<Path>();
+    }
+}
