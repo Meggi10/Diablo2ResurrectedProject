365d72e3dc4e67295a71839c73cff2a22fe20354
Magdalena Leśniewska <119002735+Meggi10@users.noreply.github.com>
2025-06-15 00:04:50 +0200

    Update projektu - 14.06.2025
    15.06.2025 - jak wrzucam projekt na git'a.
    Powrót do oryginału programu (kopia). Dodano metodę TileGraphic, która powinna zczytać grafikę TileSet'ów oraz zmodyfikowano lekko LoadStructure.
    
    Na ten moment do rozwiązania jest problem wyjątku: System.NullReferenceException w funkcji DecodeTileStage1:
    (newTile.Material.Flags =(Material.MaterialFlags)reader.ReadInt16();).
    Próba pomięcie tej linijki spowodowała, że program się zamroźnił, więc raczej ten pomysł odpada na ten moment.
    
    Z tego co udało się wywnioskować to Tiles są zczytywane, ale ich wartości zawsze są null. Natomaist Direction już się pojawia jako sensowne liczby (0, 1, 2, 3, 4, 5).
    
    TODO: Znaleźć sposób na dość nietypowy wyjątek bez błedu/ów (Material). Pominięcie kończy się niepowodzeniem (zamrożenie całego programu).

---
+++ Diablo2RProject/TPath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diablo2RProject
+{
+    public class TPath
+    {
+        public Vector2 Position { get; set; }
+        public int Action {get; set; }
+    }
+}
---
+++ Diablo2RProject/Version.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diablo2RProject
+{
+    public enum Version : Int32
+    {
+        versionEncodesFiles = 3,
+        versionEncodesFloorLayers = 4,
+        versionSimpleLayerHigh = 4,
+        versionEncodesWallLayers = 16,
+        versionEncodesAct = 8,
+        versionEncodesSubtitutionLayers = 10,
+        versionEncodesSubtitutionGroups = 12,
+        versionEncodesNpcs = 14,
+        versionEncodesNpcExtraData = 15,
+        versionHasUnknownBytes2 = 18,
+        versionUnknowBytes1Low = 9,
+        versionUnknowBytes1High = 13
+    }
+
+    public static class VersionExtensions
+    {
+        public static bool EncodeAct(this Version version)
+        {
+            return version >= Version.versionEncodesAct;
+        }
+
+        public static bool EncodeFiles(this Version version)
+        {
+            return version >= Version.versionEncodesFiles;
+        }
+
+        public static bool EncodeFloorLayers(this Version version)
+        {
+            return version >= Version.versionEncodesFloorLayers;
+        }
+
+        public static bool EncodeWallLayers(this Version version)
+        {
+            return version >= Version.versionEncodesWallLayers;
+        }
+
+        public static bool HasUnknownBytes1(this Version version)
+        {
+            return version >= Version.versionUnknowBytes1Low &&
+                   version <= Version.versionUnknowBytes1High;
+        }
+
+        public static bool HasUnknownBytes2(this Version version)
+        {
+            return version >= Version.versionHasUnknownBytes2;
+        }
+
+        public static bool EncodeSubstitutionLayers(this Version version)
+        {
+            return version >= Version.versionEncodesSubtitutionLayers;
+        }
+
+        public static bool EncodeSubstitutionGroups(this Version version)
+        {
+            return version >= Version.versionEncodesSubtitutionGroups;
+        }
+
+        public static bool EncodeNPCs(this Version version)
+        {
+            return version >= Version.versionEncodesNpcs;
+        }
+
+        public static bool EncodeNPCExtraData(this  Version version)
+        {
+            return version >= Version.versionEncodesNpcExtraData;
+        }
+
+        public static bool EncodeSimpleLayers(this Version version)
+        {
+            return version >= Version.versionSimpleLayerHigh;
+        }
+    }
+}
