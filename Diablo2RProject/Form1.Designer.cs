649d47c1a4e1e9169ad56b367f7a26545304b2e7
Magdalena Leśniewska <119002735+Meggi10@users.noreply.github.com>
2025-06-25 14:13:30 +0200

    Diablo 2 Project - Update projektu 25.06.2025
    Zmodyfikowano funckję LoadStructure w Form1.cs. Dokonano zmian w klasach DS1.cs oraz DT1.cs.
    Co udało się osiągnąć na ten moment: wiadomo, że przy ładowaniu plików *.ds1 odczytywane są pliki *.dt1,które są przypisane do tego pliku w tym samym folderze. Zmiana rozszerzenia z *.tg1 na *.dt1 pozwoliła to osiągnąć. Ale to jedynie przed dodaniem DecodeTileGraphics, które może stanowić główny problem działa programu. Wiadomo, że działa rysowanie poprzez kolor, czyli tzw. backup jeśli grafika do Tile'a by się nie wczytała. 
    
    TODO: zmienić w klasie GfxDecode.cs aby funkcja DecodeTileGraphics dekodowała na razie pojedyńczy kafelek (żeby można było już coś zobaczyć) zamiast wszystkich, żeby procesor nie osiągał 100% użycia przy rysowaniu (plus poprawić błędy w tej funkcji). Spróbować zlikwidować (zamienić) pętlę foreach po tile (potencjalnie tam może leżeć probelm).

---
+++ Diablo2RProject/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Diablo2RProject
+{
+    partial class DiabloForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.MapView = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MapView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(33, 23);
+            this.Button1.Margin = new System.Windows.Forms.Padding(0);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "LoadMap";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // MapView
+            // 
+            this.MapView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MapView.Location = new System.Drawing.Point(33, 59);
+            this.MapView.Name = "MapView";
+            this.MapView.Size = new System.Drawing.Size(702, 523);
+            this.MapView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.MapView.TabIndex = 1;
+            this.MapView.TabStop = false;
+            this.MapView.Click += new System.EventHandler(this.MapView_Click);
+            // 
+            // DiabloForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(747, 597);
+            this.ControlBox = false;
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.MapView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DiabloForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.DiabloForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MapView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox MapView;
+    }
+}
+
---
+++ Diablo2RProject/Subtile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diablo2RProject
+{
+    public class Subtile
+    {
+        public bool BlockWalk {  get; set; }
+        public bool BlockLOS { get; set; }
+        public bool BlockJump { get; set; }
+        public bool BlockPlayerWalk { get; set; }
+        public bool Unknown1 { get; set; }
+        public bool BlockLight { get; set; }
+        public bool Unknown2 { get; set; }
+        public bool Unknown3 { get; set; }
+
+        private void Combine(Subtile other)
+        {
+            BlockWalk = BlockWalk || other.BlockWalk;
+            BlockLOS = BlockLOS || other.BlockLOS;
+            BlockJump = BlockJump || other.BlockJump;
+            BlockPlayerWalk = BlockPlayerWalk || other.BlockPlayerWalk;
+            Unknown1 = Unknown1 ||other.Unknown1;
+            BlockLight = BlockLight || other.BlockLight;
+            Unknown2 = Unknown2 || other.Unknown2;
+            Unknown3 = Unknown3 || other.Unknown3;
+        }
+
+        private void NewSubtileFlags(byte data)
+        {
+            BlockWalk = (data & 1) == 1;
+            BlockLOS = (data & 2)== 2;
+            BlockJump = (data & 4 )== 4;
+            BlockPlayerWalk = (data & 8) == 8;
+            Unknown1 = (data & 16) == 16;
+            BlockLight = (data & 32) == 32;
+            Unknown2 = (data & 64) == 64;
+            Unknown3 = (data & 128) == 128;
+        }
+
+        public Subtile(byte data)
+        {
+            NewSubtileFlags(data);
+        }
+    }
+}
